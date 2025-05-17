b972478c5d7cae67917aa50d1bf47561361d2a1d
Mike EJ Redonndo <mikeejredonndo@Marcs-MacBook-Pro.local>
2025-05-17 01:48:08 -0700

    initial commit automation prct1

---
+++ src/main/java/helper/Runner.java
@@ -0,0 +1,12 @@
+package helper;
+
+public class Runner {
+    // helper method
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) {
+        print("Hello world!");
+    }
+}
