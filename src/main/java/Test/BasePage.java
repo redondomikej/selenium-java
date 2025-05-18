b17ad45409c99f84f96ae12f23bb37665b51a999
Mike EJ Redonndo <redondomikej@gmail.com>
2025-05-18 07:31:29 -0700

    safari driver comment

--- src/main/java/Test/BasePage.java
+++ src/main/java/Test/BasePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class BasePage {
     public static void main (String[] args){
+        //BrowserConfig.BrowserType browser = BrowserConfig.BrowserType.safari;
         BrowserConfig.BrowserType browser = BrowserConfig.BrowserType.chrome;
         boolean isHeadless = false ;
         WebDriver driver = BrowserConfig.getDriver(browser,isHeadless);
