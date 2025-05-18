2d13e6223010151d46415773de22a95c8c409223
Mike EJ Redonndo <mikeejredonndo@Marcs-MacBook-Pro.local>
2025-05-18 02:53:03 -0700

    add BasePage to navigate the main url

---
+++ src/main/java/Test/BasePage.java
@@ -0,0 +1,13 @@
+package Test;
+
+import helper.BrowserConfig;
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+    public static void main (String[] args){
+        BrowserConfig.BrowserType browser = BrowserConfig.BrowserType.chrome;
+        boolean isHeadless = false ;
+        WebDriver driver = BrowserConfig.getDriver(browser,isHeadless);
+        driver.get("https://practicetestautomation.com");
+    }
+}
