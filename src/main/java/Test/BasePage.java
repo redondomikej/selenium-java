f664a241beb315ef2b08714568273dd0f4231b37
Mike Redondo <redondomikej@gmail.com>
2025-05-20 09:06:13 +0800

    try3

--- src/main/java/Test/BasePage.java
+++ src/main/java/Test/BasePage.java
@@ -22,7 +22,7 @@ public class BasePage {
         loginPage.Login("student","Password123");
         loginPage.submitBtn();
         loginPage.logoutBtn();
-        log("try2");
+        log("try3");
 
         //menuNav.courses();
         //menuNav.blog();
